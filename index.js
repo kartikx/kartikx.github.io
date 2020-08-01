71eb880705883675fc170ba9c5052c6851403b1c
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-01 08:55:33 +0530

    BFS implementation

--- index.js
+++ index.js
@@ -71,3 +71,40 @@ function markToVisit(val) {
     gridCells[val].classList.add('grid-cell-tovisit');
 }
 
+
+
+async function bfs(startNode, endNode, animationTime) {
+    const step = [-gridCols, gridCols, -1, 1];
+    const stepX = [-1 , 1, 0, 0];
+    const stepY = [0, 0, -1, 1];
+
+    let queue = [];
+    let [visited, pred] = getVisited();
+
+    queue.push(startNode);
+    visited[startNode] = true;
+
+    while (queue.length > 0) {
+        let currNode = queue.shift();
+        if (currNode != startNode && currNode != endNode)
+            markVisited(currNode);
+        if (currNode == endNode) {
+            printPath(endNode, pred, animationTime*6);
+            break;
+        }
+        for (let c = 0; c < 4; c++) {
+            let nextNode = currNode + step[c];
+            let x = Math.floor(currNode/gridCols) + stepX[c];
+            let y = Math.floor(currNode%gridCols) + stepY[c];
+
+            if (isValid(x, y) && !visited[nextNode]){
+                visited[nextNode] = true;
+                if (nextNode != endNode)
+                    markToVisit(nextNode);
+                queue.push(nextNode);
+                pred[nextNode] = currNode;
+                await sleep(animationTime);
+            }
+        }
+    }
+}
